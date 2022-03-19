b25bab7ee0392ee12c8d5ebe78e47dd8407b206c
ahmedmada <ahmedmada1996@gmail.com>
2022-03-19 08:45:24 +0200

    build architecture

--- app/src/main/java/com/app/weather/presentation/main/MainActivity.java
+++ app/src/main/java/com/app/weather/presentation/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.app.weather;
+package com.app.weather.presentation.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.app.weather.R;
 
 public class MainActivity extends AppCompatActivity {
 
